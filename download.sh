4f02b3c889222bf852b1b608231d593e40104f38
paulchen <paulchen@rueckgr.at>
2020-12-13 23:17:50 +0100

    change download URL for rbb to avoid 404 errors at some point of time during streaming

--- download.sh
+++ download.sh
@@ -13,13 +13,15 @@ fi
 SIMPLE=0
 SEGMENT_TIME=6
 case "$1" in
-	daserste) URL='https://mcdn.daserste.de/daserste/de/master.m3u8'                        ;;
-	zdf)      URL='https://zdf-hls-15.akamaized.net/hls/live/2016498/de/high/master.m3u8'   ;;
-	zdfneo)   URL='https://zdf-hls-16.akamaized.net/hls/live/2016499/de/high/master.m3u8'   ;;
+	daserste) URL='https://mcdn.daserste.de/daserste/de/master.m3u8' ;;
+	zdf)      URL='https://zdf-hls-15.akamaized.net/hls/live/2016498/de/high/master.m3u8' ;;
+	zdfneo)   URL='https://zdf-hls-16.akamaized.net/hls/live/2016499/de/high/master.m3u8' ;;
 	3sat)     URL='https://zdf-hls-18.akamaized.net/hls/live/2016501/dach/high/master.m3u8' ;;
-	orf1)     URL='https://orf1.mdn.ors.at/out/u/orf1/qxb/manifest.m3u8'                    ;;
-	orf2)     URL='https://orf2.mdn.ors.at/out/u/orf2/qxb/manifest.m3u8'                    ;;
-	orf3)     URL='https://orf3.mdn.ors.at/out/u/orf3/qxb/manifest.m3u8'                    ;;
+	orf1)     URL='https://orf1.mdn.ors.at/out/u/orf1/qxb/manifest.m3u8' ;;
+	orf2)     URL='https://orf2.mdn.ors.at/out/u/orf2/qxb/manifest.m3u8' ;;
+	orf3)     URL='https://orf3.mdn.ors.at/out/u/orf3/qxb/manifest.m3u8' ;;
+	rbb)      URL='https://rbblive-lh.akamaihd.net/i/rbb_brandenburg@349369/index_5_av-p.m3u8?sd=4&set-segment-duration=responsive&rebase=on' ;;
+	mdr)      URL='https://mdrtvsnhls.akamaized.net/hls/live/2016928/mdrtvsn/master.m3u8' ;;
 	*)
 		# TODO extend by more stations
 		echo "Unknown station $1"
